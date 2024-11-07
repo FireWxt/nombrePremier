7c5e2b1d9cbd0fb029b16ff66d3f8dc1e169f896
Simon Daconceicao <semiaosimon@gmail.com>
2024-11-07 15:57:05 +0100

    Ajout de la fonction pour gérer le multithread et ouvrir le fichier

--- NombrePremier/NombrePremier.cpp
+++ NombrePremier/NombrePremier.cpp
@@ -34,7 +34,74 @@ bool estPremier(int valeur)
 
 
 
+void trouverNombresPremiers(int id, int fin, int threadsUtilise)
+{
+
+    std::vector<int> resultatsLocaux;
+    for (int i = id; i <= fin; i += threadsUtilise)
+    {
+        if (estPremier(i))
+        {
+            resultatsLocaux.push_back(i);
+        }
+    }
+
+    listePremier.insert(listePremier.end(), resultatsLocaux.begin(), resultatsLocaux.end());
+
+}
+
+
+void calculerNombresPremiers(int threadsUtilise, int fin)
+{
+
+
+    std::vector<std::thread> listeAsync;
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < threadsUtilise; i++)
+    {
+        listeAsync.push_back(std::thread(trouverNombresPremiers, i, fin, threadsUtilise));
+    }
+
+    for (auto& t : listeAsync)
+    {
+        t.join();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    std::cout << elapsed.count() << " microsecondes  " << " \t";
+
+
+    std::ofstream fichier("nombres_premiers.txt");
+    fichier.clear();
+    if (fichier.is_open())
+    {
+        for (int premier : listePremier)
+        {
+            fichier << premier << std::endl;
+        }
+        fichier.close();
+        std::cout << "Les nombres premiers ont ete enregistres dans 'nombres_premiers.txt'." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Erreur : impossible d'ouvrir le fichier." << std::endl;
+    }
+}
+
 int main()
 {
 
+    int threadsUtilise = std::thread::hardware_concurrency();
+
+    calculerNombresPremiers(1, 10000);;
+    calculerNombresPremiers(threadsUtilise, 10000);
+    calculerNombresPremiers(1, 100000);
+    calculerNombresPremiers(threadsUtilise, 100000);
+    calculerNombresPremiers(1, 1000000);
+    calculerNombresPremiers(threadsUtilise, 1000000);
+
+    return 0;
 }
